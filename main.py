a996acf6ff8478ac405be1652cacd504c3bbb0c5
heelas.uk <alfred.heelas@gmail.com>
2025-03-21 14:38:44 +0000

    intial

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def login_screen():
+    st.header("This app is private.")
+    st.subheader("Please log in.")
+    st.button("Log in with Microsoft", on_click=st.login)
+
+if not st.experimental_user.is_logged_in:
+    login_screen()
+else:
+    st.header(f"Welcome, {st.experimental_user.name}!")
+    st.button("Log out", on_click=st.logout)
